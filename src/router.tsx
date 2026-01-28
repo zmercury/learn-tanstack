69e850a6154dea9489ec2e2d949fe2a710ed3d58
zmercury <messiniki@outlook.com>
2026-01-28 10:50:06 +0545

    feat: created a simple route using tanstack start

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+// src/router.tsx
+import { createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+export function getRouter() {
+  const router = createRouter({
+    routeTree,
+    scrollRestoration: true,
+  })
+
+  return router
+}
